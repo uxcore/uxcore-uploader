4cf2a516e1a97016976a3ac89ac9f220175ca0fa
yanbingbing <me@yanbingbing.com>
2015-12-29 16:24:53 +0800

    move presets to webpack.config

--- webpack.dev.js
+++ webpack.dev.js
@@ -14,7 +14,10 @@ module.exports = {
         loaders: [{
             test: /\.jsx?$/,
             exclude: /node_modules/,
-            loader: 'babel-loader'
+            loader: 'babel-loader',
+            query: {
+                "presets": ["es2015", "react"]
+            }
         }, {
             test: /\.css$/,
             loader: 'style-loader!css-loader'
